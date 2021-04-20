be69756c81795aa334a9e91947df1d07c8eca182
gseek27 <goalseek27@gmail.com>
2021-04-20 16:58:00 -0400

    itslitapp

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+ echo 'itslit.app' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:gseek27/gseek27.github.io.git master
+# git push -f git@github.com:gseek27/gseek27.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
